7da3a8d7ea2bbb829e3d474f40c89c6561722731
Alok Singh <salok2857@gmail.com>
2025-09-17 20:54:11 +0530

    using generic in fist using Pick

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
+// interface User {
+//   name: string;
+//   age: number;
+// }
+
+// function sumOfAge(user1: User, user2: User) {
+//   return user1.age + user2.age;
+// }
+
+// const answer = sumOfAge({ name: "Alice", age: 30 }, { name: "Bob", age: 25 });
+
+// console.log(answer);
+
 interface User {
+  id: string;
   name: string;
   age: number;
+  email: string;
+  password: string;
 }
 
-function sumOfAge(user1: User, user2: User) {
-  return user1.age + user2.age;
-}
+type Updateprop = Pick<User, "name" | "age" | "email">;
 
-const answer = sumOfAge({ name: "Alice", age: 30 }, { name: "Bob", age: 25 });
-
-console.log(answer);
+function updateUser(Updateprop: Updateprop) {}
