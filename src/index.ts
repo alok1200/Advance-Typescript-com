323bbe611129cda89a41ed5023241caf16ff85be
Alok Singh <salok2857@gmail.com>
2025-09-18 16:52:18 +0530

    this is how we using object in typescript

--- src/index.ts
+++ src/index.ts
@@ -58,34 +58,73 @@
 
 // second way usinf Partial
 
-interface Employee {
-  id: number;
-  name: string;
-  address: string;
-  mobile: number;
-}
+// interface Employee {
+//   id: number;
+//   name: string;
+//   address: string;
+//   mobile: number;
+// }
 
-type EmployeeProp = Pick<Employee, "id" | "name" | "address">;
+// type EmployeeProp = Pick<Employee, "id" | "name" | "address">;
 
-type UpdateEmployee = Partial<EmployeeProp>;
+// type UpdateEmployee = Partial<EmployeeProp>;
 
-function updateEmployee(EmployeeProp: UpdateEmployee) {}
+// function updateEmployee(EmployeeProp: UpdateEmployee) {}
 
 // Readonly
 
-type User = {
-  readonly name: string;
-  readonly age: number;
-  readonly email: string;
-  readonly password: string;
-};
+// type User = {
+//   readonly name: string;
+//   readonly age: number;
+//   readonly email: string;
+//   readonly password: string;
+// };
 
 // const user: readonly <User> = {. this is also work same as readonly
-const user: User = {
-  name: "akash",
-  age: 23,
-  email: "akash@gmail.com",
-  password: "123456",
-};
+// const user: User = {
+//   name: "akash",
+//   age: 23,
+//   email: "akash@gmail.com",
+//   password: "123456",
+// };
+
+// use case of readonly is configraction any api key like (gpt api key etc)
 
 // user.name = "abjcnd"
+
+//Records and Map
+
+type User = {
+  id: number;
+  name: string;
+};
+
+// type Users = {              // this is second way to right this code
+//     [key: string]: {
+//   id: number;
+//   name: string;
+// }
+// }
+
+type Users = {
+  [key: string]: User;
+};
+
+const users: Users = {
+  user1: {
+    id: 1,
+    name: "akash",
+  },
+};
+
+const newUser: Users = {
+  "abcd@12": {
+    id: 1,
+    name: "akash",
+  },
+
+  "abcd@123": {
+    id: 2,
+    name: "vivek",
+  },
+};
